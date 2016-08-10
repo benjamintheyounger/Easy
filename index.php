cce54d9cc7b11b1a9428bb379d04aec055b7bbe2
benjamintheyounger <Benjaminartik@gmail.com>
2016-08-10 00:20:30 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>EasyTrader</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <link rel="stylesheet" href="vistas/css/estilos.css">
+        <link rel="stylesheet" href="vistas/css/fontello.css">
+        <link rel="icon" href="vistas/img/carrito-de-compras.png" />
+        
+    </head>
+    <body>
+        <main id="main">      
+            <?php include("./vistas/header.php"); ?>
+            <?php include("./vistas/banner.php"); ?>
+            <?php include("./vistas/info.php"); ?>
+            <?php include("./vistas/footer.php"); ?>
+            <?php include("./vistas/login.php"); ?>
+            <?php include("./vistas/nuevousuario.php"); ?>
+            <?php include("./vistas/contacto.php"); ?>7
+        </main>
+    </body>
+</html>
